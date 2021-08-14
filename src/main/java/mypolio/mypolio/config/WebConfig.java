318f8ff3158ae9f45aa38d9e620c5b4133410dcc
kye8524 <kye8314@gmail.com>
2021-08-14 15:06:23 +0900

    add cors

--- src/main/java/mypolio/mypolio/config/WebConfig.java
+++ src/main/java/mypolio/mypolio/config/WebConfig.java
@@ -1,31 +1,15 @@
-//package mypolio.mypolio.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .exceptionHandling()
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/user/signup").permitAll()
-//                .antMatchers("/user/login").permitAll()
-//                .antMatchers("/user/verify/**").permitAll()
-//                .antMatchers("/oauth/**").permitAll()
-//                .antMatchers("/test/user").hasRole("USER")
-//                .antMatchers("/test/admin").hasRole("ADMIN")
-//                .anyRequest().authenticated();
-//    }
-//}
+package mypolio.mypolio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
